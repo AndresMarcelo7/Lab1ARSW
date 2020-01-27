b8587936637dff161d4da350c28f210ee7d4eb0b
Sim0no <german.marin@mail.escuelaing.edu.co>
2020-01-27 15:03:02 -0500

    Añadiendo parte 2

---
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -0,0 +1,44 @@
+package edu.eci.arsw.primefinder;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+                int ini = 0;
+                long startTime= System.currentTimeMillis();
+                ArrayList<PrimeFinderThread> hilos= new ArrayList<PrimeFinderThread>(); 
+		for (int threads = 0; threads <=2; threads++){
+                    PrimeFinderThread pft=new PrimeFinderThread(ini, ini+9999999);
+                    ini += 10000000;
+                    pft.start();
+                    hilos.add(pft);
+                    //Si se desean imprimir los numeros primos en orden desdpcumetar la linea siguiente
+                    
+                    //pft.join();
+                }
+                while(true){
+                    int v=0;
+                    if (System.currentTimeMillis() - startTime >= 5000){
+                        for (PrimeFinderThread hilo:hilos){
+                            hilo.setSuspender(true);
+                            v+=hilo.getVeces();
+                        }                      
+                        System.out.println("Numero de primos encontrados: "+Integer.toString(v));
+                        System.out.println("Presione enter para continuar...");
+                        Scanner entrada = new Scanner(System.in);
+                        entrada.nextLine();
+                        entrada.close();
+                        for (PrimeFinderThread hilo:hilos){
+                            hilo.despertar();
+                        }  
+                        break;
+                    }
+                }
+		
+		
+		
+	}
+	
+}
---
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -0,0 +1,83 @@
+package edu.eci.arsw.primefinder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PrimeFinderThread extends Thread{
+
+	
+	int a,b,veces;
+
+    public int getVeces() {
+        return veces;
+    }
+
+    public void setVeces(int veces) {
+        this.veces = veces;
+    }
+	boolean suspender;
+    public boolean isSuspender() {
+        return suspender;
+    }
+
+    public void setSuspender(boolean suspender) {
+        this.suspender = suspender;
+    }
+	private List<Integer> primes=new LinkedList<Integer>();
+	
+	public PrimeFinderThread(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+                this.veces=0;
+                
+                
+	}
+
+	public synchronized void run(){
+                long startTime= System.currentTimeMillis();
+                boolean flag =false;
+		for (int i=a;i<=b;i++){	
+                        
+                        while (isSuspender()){
+                            try {
+                                wait();
+                                System.out.println("prueba");
+                            } catch (InterruptedException ex) {
+                                Logger.getLogger(PrimeFinderThread.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+			if (isPrime(i)){                            
+				primes.add(i);
+				System.out.println(i);
+                                this.veces+=1;
+			}                        
+		}
+		
+		
+	}
+	
+	boolean isPrime(int n) {
+	    if (n%2==0) return false;
+	    for(int i=3;i*i<=n;i+=2) {
+	        if(n%i==0)
+	            return false;
+	    }
+	    return true;
+	}
+
+	public List<Integer> getPrimes() {
+		return primes;
+	}
+
+    synchronized void despertar() {
+        this.suspender=false;
+        notify();
+    }
+	
+	
+	
+	
+}
